1a66b4cbb6b8e48d5383d7e3aab4d86c4945ab12
Marta Slomiak private <martaslomiak@gmail.com>
2026-01-31 22:54:37 +0100

    Add a starter to DestinAI

---
+++ DestinAI/src/main/java/com/example/demo/DestinaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestinaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DestinaiApplication.class, args);
+	}
+
+}
---
+++ DestinAI/src/test/java/com/example/demo/TestDestinaiApplication.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestDestinaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(DestinaiApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
